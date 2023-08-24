bcb1dfbcd7e4bc8c84b4e8ce790e1b82ca699a62
valentino <v.arballo@itecriocuarto.org.ar>
2023-08-24 17:47:02 -0300

    format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import logoitec from './images/image.png'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        className='frecodecamp-logo' 
+        src = {logoitec}
+        alt = 'Logo del Itec'
+        />
+      </div>
+      <div className='contenedor-contador'>
+        
+      </div>
     </div>
   );
 }
